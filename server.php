1c0f162ae2f903a8fd7e2796cf4be4f465a66366
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-08 16:05:34 +0200

    Upgraded server

--- server.php
+++ server.php
@@ -1,1 +1,18 @@
 <?php 
+
+// Leggi la lista di attività dal file JSON
+$tasks = json_decode(file_get_contents('tasks.json'), true);
+
+// Aggiungi il nuovo task alla lista
+$newTask = array(
+  'task' => $_POST['task'],
+  'completed' => false
+);
+array_push($tasks, $newTask);
+
+// Salva la lista di attività aggiornata nel file JSON
+file_put_contents('tasks.json', json_encode($tasks));
+
+// Ritorna il nuovo task come risposta alla richiesta POST
+header('Content-Type: application/json');
+echo json_encode($newTask);
