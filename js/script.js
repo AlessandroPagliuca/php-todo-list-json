24aa766e0b1f97305ec9bc3065e8d97c37beb205
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-08 16:04:47 +0200

    Upgraded addTask

--- js/script.js
+++ js/script.js
@@ -3,13 +3,35 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
+            tasks:[],
+            newTask: '',
 
         }
     },
     methods:{
+        addTask() {
+            if (this.newTask !== '') {
+                this.tasks.push({
+                    task: this.newTask,
+                });
 
+                this.newTask = '',
+                console.log(this.newTask);
+            }
+            // Effettua la richiesta POST al server per aggiungere il nuovo task
+            axios.post('../tasks.json', { task: this.newTask })
+            .then(res => {
+            // Aggiunge il nuovo task alla lista di attività
+            this.tasks.push(res.data);
+            // Resetta il campo di input
+            //this.newTask = '';
+            })
+            .catch(error => {
+            console.error(error);
+            })
+        }
     },
     mounted(){
-
+        this.addTask();
     },
 }).mount('#app');
